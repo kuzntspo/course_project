37c0a6f09d632dcf1a08f360444d1d118df9a20e
kuzntspo <108883547+kuzntspo@users.noreply.github.com>
2023-01-13 19:42:53 +0300

    creating a search bar on the main page

--- main_page.php
+++ main_page.php
@@ -2,9 +2,10 @@
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="main_page.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet">
     <title>Аттракционы Москвы</title>
 </head>
@@ -17,7 +18,12 @@
         </ul>
     </header>
     <main>
-    
+    <div class="square-block">
+        <form>
+            <input type="text" placeholder="Искать здесь...">
+            <button type="submit"></button>
+        </form>
+    </div>
     </main>
     <footer></footer>
 </body>
