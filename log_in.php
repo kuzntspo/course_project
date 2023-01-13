c790a0d563970c1f0007c8ab3428bf81cb3966b4
kuzntspo <108883547+kuzntspo@users.noreply.github.com>
2023-01-14 00:37:53 +0300

    icon setting

--- log_in.php
+++ log_in.php
@@ -7,6 +7,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="img/logo.png" type="image/x-icon" />
     <title>Аттракционы Москвы</title>
     
 </head>
